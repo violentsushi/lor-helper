9d88a91ce70a285b29c4b0b4cc70e1db7bd0b764
violentsushi <violentsushi@gmail.com>
2025-05-30 17:28:11 -0500

    home page content information is mostly done

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,75 @@
-// todo fix sprite, just need to get sprite image somewhere
 export const transformDescription = (description: string, name?: string) => 
-    description.replaceAll(/<style=(\w+)>([A-Za-z0-9:' ]+)<\/style>/g, (_substring, style: string, text: string) => {
+    description.replaceAll(/<link=keyword.([A-Za-z ]+)><sprite name=(\w+)>([A-Za-z0-9<>=/ ]+)<\/link>/g, (_substring, _keyword: string, iconName: string, text: string) =>
+        `<span><sprite name=${iconName}>${text}</span>`
+    ).replaceAll(/<style=(\w+)>([A-Za-z0-9:' ]+)<\/style>/g, (_substring, style: string, text: string) => {
         if (text === 'CardRef') {
+            style = 'AssociatedCard'
             switch(name) {
-                case "Absolution": text = text.replace("CardRef", "Warlord's Hoard"); 
+                case "Absolution":
+                    text = "Warlord's Hoard";
                     break;
-                case "The Time Keeper": text = text.replace("CardRef", "The Vault");
+                case "The Time Keeper":
+                    text = "The Vault";
                     break;
                 default:
                     break;
             }
-            style = 'AssociatedCard'
         }
 
-        return `<span class="${style.toLowerCase()}">${text}</span>`}
-    ).replaceAll(/<color=([A-Za-z0-9#]+)>([A-Za-z0-9:' ]+)<\/color>/g, (_substring, _style: string, text: string) =>
+        if (style === "AssociatedCard") {
+            return `<a href="https://wiki.leagueoflegends.com/en-us/LoR:${text.replaceAll(" ", "_")}" class="${style.toLowerCase()}" target="_blank">${text}</a>`
+        }
+        
+        return `<span class="${style.toLowerCase()}">${text}</span>`
+    }).replaceAll(/<color=([A-Za-z0-9#]+)>([A-Za-z0-9:' ]+)<\/color>/g, (_substring, _color: string, text: string) =>
         `<span class="keyword">${text}</span>`
-    )
+    ).replaceAll(/<sprite name=(\w+)>/g, (_substring, iconName:string) => {
+        let src = `https://wiki.leagueoflegends.com/en-us/images`
+
+        // fix name
+        switch(iconName) {
+            case "SkillMark":
+                iconName = "Skill";
+                break;
+            case "Lurker":
+                iconName = "Lurk";
+                break;
+            case "LastBreath":
+                iconName = "Last_Breath";
+                break;
+            case "SpellShield":
+                iconName = "Spellshield";
+                break;
+            case "ElementalSkill":
+                iconName = "Elemental_Skill";
+                break;
+            case "QuickStrike":
+                iconName = "QuickAttack"
+                break;
+            case "Reckless":
+                iconName = "Can't_Block"
+                break;
+            case "BlockElusive":
+                iconName = "Sharpsight"
+                break;
+            case "Immobile":
+                iconName = "Can't_Attack"
+                break;
+            default:
+                break;
+        }
+
+        switch(iconName) {
+            case "Last_Breath":
+                src += `/Keyword_${iconName}.svg`;
+                break;
+            case "Elemental_Skill":
+                src += `/thumb/Keyword_${iconName}.png/100px-Keyword_${iconName}.png`
+                break;
+            default:
+                src += `/thumb/Keyword_${iconName}_HD.png/100px-Keyword_${iconName}_HD.png`
+                break;
+        }
+
+        return `<image src="${src}" alt="${iconName} icon" width="16" class="mr-1 inline-block mb-1" />`
+    })
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,10 +20,10 @@
     <h2>Powers</h2>
     {#each powers as power}
         <div class="bg-gray-600 m-1 rounded p-1" id={power.powerCode}>
-            <img src={power.assetFullAbsolutePath} alt={power.name} width="400"/>
-            <a href={`#${power.powerCode}`}><h3>{power.name}</h3></a>
-            <p>{power.rarity}</p>
-            <p>{@html transformDescription(power.description, power.name)}</p>
+            <img src={power.assetFullAbsolutePath} alt={power.name} width="400" />
+            <h3><a href={`#${power.powerCode}`} class="mr-1">{power.name}</a></h3>
+            <div class="flex items-center"><img src={`https://wiki.leagueoflegends.com/en-us/images/thumb/LoR_${power.rarityRef}_icon.png/100px-LoR_${power.rarityRef}_icon.png`} alt={power.rarity} width="16" class="mr-1" />{power.rarity}</div>
+            <div><span>{@html transformDescription(power.description, power.name)}</span></div>
         </div>
     {/each}
 </div>
@@ -33,9 +33,9 @@
     {#each relics as relic}
         <div class="bg-gray-600 m-1 rounded p-1" id={relic.relicCode}>
             <img src={relic.assetFullAbsolutePath} alt={relic.name} width="400"/>
-            <a href={`#${relic.relicCode}`}><h3>{relic.name}</h3></a>
-            <p>{relic.rarity}</p>
-            <p>{@html transformDescription(relic.description, relic.name)}</p>
+            <h3><a href={`#${relic.relicCode}`} class="mr-1">{relic.name}</a></h3>
+            <div class="flex items-center"><img src={`https://wiki.leagueoflegends.com/en-us/images/thumb/LoR_${relic.rarityRef}_icon.png/100px-LoR_${relic.rarityRef}_icon.png`} alt={relic.rarity} width="16" class="mr-1" />{relic.rarity}</div>
+            <div><span>{@html transformDescription(relic.description, relic.name)}</span></div>
         </div>
     {/each}
 </div>
@@ -45,9 +45,9 @@
     {#each items as item}
         <div class="bg-gray-600 m-1 rounded p-1" id={item.itemCode}>
             <img src={item.assetFullAbsolutePath} alt={item.name} width="400"/>
-            <a href={`#${item.itemCode}`}><h3>{item.name}</h3></a>
-            <p>{item.rarity}</p>
-            <p>{@html transformDescription(item.description, item.name)}</p>
+            <h3><a href={`#${item.itemCode}`} class="mr-1">{item.name}</a></h3>
+            <div class="flex items-center"><img src={`https://wiki.leagueoflegends.com/en-us/images/thumb/LoR_${item.rarityRef}_icon.png/100px-LoR_${item.rarityRef}_icon.png`} alt={item.rarity} width="16" class="mr-1" />{item.rarity}</div>
+            <div><span>{@html transformDescription(item.description, item.name)}</span></div>
         </div>
     {/each}
 </div>
